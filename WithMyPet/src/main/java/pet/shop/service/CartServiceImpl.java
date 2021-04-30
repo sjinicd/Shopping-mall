df47661ca09532edb53a8c757290c811b7eaab91
Jinny Sung <75344406+sjinicd@users.noreply.github.com>
2021-04-30 16:41:56 +0900

    Update CartServiceImpl.java

--- WithMyPet/src/main/java/pet/shop/service/CartServiceImpl.java
+++ WithMyPet/src/main/java/pet/shop/service/CartServiceImpl.java
@@ -30,7 +30,6 @@ public class CartServiceImpl implements CartService {
 	@Override
 	public List<Cart> findCartProducts(long product_code) throws Exception {
 		//테이블에 추가하기 전에 동일한 상품 번호의 개수를 조회
-		log.info("listCart 잘나옴: "+product_code);
 		List<Cart> c = cartMapper.selectCountInCart(product_code);
 		if(c !=null) log.info("cart 널아님:" +c);
 		return cartMapper.selectCountInCart(product_code);
@@ -41,25 +40,20 @@ public class CartServiceImpl implements CartService {
 	}
 	@Override
 	public ArrayList<Pay> selectPay(int member_number) throws Exception {
-		log.info("pay 잘나옴:" +member_number);
 		ArrayList<Pay> pay = cartMapper.selectPay(member_number);
-		log.info("pay값: "+pay);
 		return cartMapper.selectPay(member_number);
 	}
 	@Override
 	public void updatePayS(Pay pay) throws Exception {
-		log.info("pay 잘나옴:" +pay);
 		cartMapper.updateOrder(pay);
 	}
 	@Override
 	public Pay selectPayOne(int member_number) throws Exception {
 		Pay pay = cartMapper.selectPayOne(member_number);
-		System.out.println("##"+pay);
 		return pay;
 	}
 	@Override
 	public void insertOrderSu(OrderSu ordersu) throws Exception {
-		log.info("@@service ordersu값: "+ordersu);
 		cartMapper.insertOrderSu(ordersu);
 		
 	}
